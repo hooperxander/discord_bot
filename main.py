b683a4b88369d0c530571c89c9dc895b8fa3e64c
trenthudgens <hudgens.trent@gmail.com>
2022-01-27 14:04:05 -0700

    added deez message

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    
+    if message.content.startswith('$deez'):
+        await message.channel.send("Aidan Is Gay! We Are So Proud Of Aidan For Coming Out As Gay!")
 
 client.run('OTM2MzU4NDUyNzkwNDMxNzU0.YfMBww._ME6JBvomUkhYtDnfgZfvfzFrMg')
