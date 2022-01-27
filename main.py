3a42b8dbe9e4576a48822884b046960aa0fea246
trenthudgens <hudgens.trent@gmail.com>
2022-01-27 15:26:49 -0700

    GLOGOOBOGBOOBLOGBL

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('maro hello'):
         await message.channel.send('Hello!')
     
-    if message.content.startswith('$deez'):
+    if message.content.startswith('maro deez'):
         await message.channel.send("Aidan Is Gay! We Are So Proud Of Aidan For Coming Out As Gay!")
 
+    if message.content.startswith('maro pls'):
+            await message.channel.send("OOBLOOBLBLBLABOBLBLWAOOOOOOOOOO")
+            await message.channel.send("GULUUGOBOGLOOUGOUBOGOLOOGUBOGOBUOGBOOOLOBOGOO")
+            await message.channel.send("Haaah... Hah... Haaah... Hooo...")
+            await message.channel.send("glooogoboAOGOBIGOBOLGOOBOOLLOBGLOLOAOLLIBLIGGUGLGUBUULL")
 client.run(secret_token.secret_token)
